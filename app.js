3f5265291a335c1d8f0f303d2212486e962bb863
Yangjeongwon1133 <nm8271@gmail.com>
2025-06-23 16:18:32 +0900

    자료형 알아보기

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 var a = '변수값';
-console.log(a) 
+console.log(a) // print '변수값'
+
+let b = 1
+b = 2
+console.log(b) //변수 (변경가능)
+
+const c = 5
+//c = 10
+console.log(c) //상수 (변경불가)
+
+let d = 10
+console.log(d, typeof d)
+d = '문자'
+console.log(d, typeof d)
